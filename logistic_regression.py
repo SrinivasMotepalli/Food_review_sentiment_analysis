b05562d2b2ee9987cf245cf6d7cbc3419024a3a2
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2024-04-14 18:23:16 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import string
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
