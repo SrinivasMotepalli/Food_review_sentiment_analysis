fa35ef1bff85caee8bfafff25de1f0870056ee0f
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2024-04-14 21:10:53 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,14 +7,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import pickle
 from translate import Translator
+
 
 # Load the trained model
 def load_model(model_path):
