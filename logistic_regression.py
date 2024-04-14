13c2509c6c630793a1d6a196d51d9dc7094e9f4c
SrinivasMotepalli <148742392+SrinivasMotepalli@users.noreply.github.com>
2024-04-14 21:58:51 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -23,16 +23,16 @@ def load_vectorizer(vectorizer_path):
     return vectorizer
 
 languages = {
-    'en': 'English(India)',
-    'gu-IN': 'Gujarati(India)',
-    'hi-IN': 'Hindi(India)',
-    'kn-IN': 'Kannada(India)',
-    'kok-IN': 'Konkani(India)',
-    'mr-IN': 'Marathi(India)',
-    'pa-IN': 'Punjabi(India)',
-    'sa-IN': 'Sanskrit(India)',
-    'ta-IN': 'Tamil(India)',
-    'te-IN': 'Telugu(India)'
+    'en',
+    'gu-IN',
+    'hi-IN',
+    'kn-IN',
+    'kok-IN',
+    'mr-IN',
+    'pa-IN',
+    'sa-IN',
+    'ta-IN',
+    'te-IN''
 }
 
 st.title('Multilingual Comment Analyzer')
